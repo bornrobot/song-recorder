25abcc171602a36d5d55980a4d8ed5fa966c754b
bornrobot <ed@edr.so>
2019-12-30 15:02:39 +0000

    add redist queing

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
 'use strict'
 const mysql = require('mysql');
-let config = require('./config.js');
+const config = require('./config.js');
+const recorder = require('node-record-lpcm16');
+const fs = require('fs');
+const redis = require('redis');
 
-let songId = 387;
+let recording = null;
 
-const recorder = require('node-record-lpcm16')
-const fs = require('fs')
-
-let wavFile = "./audio/" + songId + ".wav";
-const file = fs.createWriteStream(wavFile, { encoding: 'binary' })
-
-recorder.record({
-  sampleRate: 44100,
-  endOnSilence: true
-})
-  .stream()
-  .pipe(file);
-
+var client = redis.createClient(36379, "127.0.0.1");
 const db = mysql.createConnection(config);
 
-console.log('Connected to database');
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
 
-let query = "UPDATE tblSongs SET wavUrl='" + wavFile + "'";
-query += " WHERE songId=" + songId;
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
 
-db.query(query, (err, result) => {
-  if (err) {
-    throw err;
+client.on("message", function (channel, message) {
+
+  console.log("Message: " + message + " on channel: " + channel + " is arrive!");
+
+  var msg = JSON.parse(message);
+
+  if(msg.Action === "StartRecording"){
+    startRecording( getWavName(msg.songId) );
+  }
+  if(msg.Action === "StopRecording"){
+    stopRecording(function() {
+      updateDatabase(msg.songId, getWavName(msg.songId));
+    });
   }
 });
 
-db.end();
+client.subscribe("notification");
+
+
+function getWavName(songId) {
+    return "./assets/wav/" + songId + ".wav";
+}
+
+function stopRecording(callback) {
+    recording.stop();
+    callback();
+}
+
+function startRecording(wavName) {
+  console.log("Start recording to " + wavName);
+
+  const file = fs.createWriteStream(wavName, { encoding: 'binary' });
+
+  recording = recorder.record({
+    sampleRate: 44100,
+    endOnSilence: false
+  });
+
+  recording.stream()
+  .pipe(file);
+}
+
+function updateDatabase(songId, wavName) {
+
+  console.log('Connected to database');
+
+  let query = "UPDATE tblSongs SET wavUrl='" + wavName + "'"; 
+  query += " WHERE songId=" + songId;
+
+  db.query(query, (err, result) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
+//db.end();
+
